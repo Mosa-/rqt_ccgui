e7207a6d9ac5e9557d3ab2b757ffc405b201d22a
Mosa <m.nurzai@gmx.de>
2014-12-19 20:22:24 +0100

    First gui design finished Problem: Sub/Pub-Problems with rqt_ccg and main node

--- include/rqt_ccgui/ccgui.h
+++ include/rqt_ccgui/ccgui.h
@@ -6,7 +6,10 @@
 #include <QWidget>
 #include <QStringList>
 #include <QIcon>
+#include <QMessageBox>
 #include <ros/package.h>
+#include <std_msgs/String.h>
+#include <ros/ros.h>
 
 namespace rqt_ccgui {
 
@@ -28,6 +31,21 @@ private:
   Ui_Form ui_;
   QWidget* widget_;
   void configureHelpButtons();
+  void activeSpeechCommand(QString command, QString data);
+  void deactiveSpeechCommand(QString command);
+  void receiveSpeechCommand(const std_msgs::StringConstPtr& cmd);
+
+private slots:
+  void clickLabelhelpBtn_timeout();
+  void clickLabelhelpBtn_Dsleeptime();
+  void clickLabelhelpBtn_DexeCount();
+  void clickLabelhelpBtn_Dmovespeed();
+  void clickLabelhelpBtn_DaccFactor();
+  void clickLabelhelpBtn_maxSpeed();
+  void clickLabelhelpBtn_DtwistFactor();
+  void clickLabelhelpBtn_DtwistSpeed();
+  void clickLabelhelpBtn_DgripperStep();
+
 };
 } // namespace
 #endif
--- src/rqt_ccgui/ccgui.cpp
+++ src/rqt_ccgui/ccgui.cpp
@@ -14,6 +14,8 @@ ccgui::ccgui()
   setObjectName("rqt_ccgui");
 }
 
+
+
 void ccgui::initPlugin(qt_gui_cpp::PluginContext& context)
 {
   // access standalone command line arguments
@@ -25,7 +27,29 @@ void ccgui::initPlugin(qt_gui_cpp::PluginContext& context)
   // add widget to the user interface
   context.addWidget(widget_);
 
+  QIcon refresh_icon;
+  std::string path = ros::package::getPath("rqt_ccgui")+"/img/redstatus.png";
+  QString icon_path(path.c_str());
+  refresh_icon.addFile(icon_path);
+
+  ui_.status->setIcon(refresh_icon);
+  ui_.status->setIconSize(QSize(21, 21));
+  ui_.status->setStyleSheet("QPushButton{border: none;outline: none;}");
+
   this->configureHelpButtons();
+  this->deactiveSpeechCommand("move");
+  this->deactiveSpeechCommand("turn");
+  this->deactiveSpeechCommand("look");
+  this->deactiveSpeechCommand("grip");
+  connect(ui_.helpBtn_timeout, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_timeout()));
+  connect(ui_.helpBtn_Dsleeptime, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_Dsleeptime()));
+  connect(ui_.helpBtn_DexeCount, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_DexeCount()));
+  connect(ui_.helpBtn_Dmovespeed, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_Dmovespeed()));
+  connect(ui_.helpBtn_DaccFactor, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_DaccFactor()));
+  connect(ui_.helpBtn_maxSpeed, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_maxSpeed()));
+  connect(ui_.helpBtn_DtwistFactor, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_DtwistFactor()));
+  connect(ui_.helpBtn_DtwistSpeed, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_DtwistSpeed()));
+  connect(ui_.helpBtn_DgripperStep, SIGNAL(clicked()), this, SLOT(clickLabelhelpBtn_DgripperStep()));
 }
 
 void ccgui::shutdownPlugin()
@@ -55,48 +79,160 @@ void triggerConfiguration()
   // Usually used to open a dialog to offer the user a set of configuration
 }*/
 
+
+void ccgui::receiveSpeechCommand(const std_msgs::StringConstPtr& cmd){
+	ROS_INFO("test test %s", cmd->data.c_str());
+}
+
+
 void ccgui::configureHelpButtons(){
 	  QIcon refresh_icon;
 	  std::string path = ros::package::getPath("rqt_ccgui")+"/img/helpbtn.png";
 	  QString icon_path(path.c_str());
 	  refresh_icon.addFile(icon_path);
 
-	  ui_.helpBtn1->setIcon(refresh_icon);
-	  ui_.helpBtn1->setIconSize(QSize(21, 21));
-	  ui_.helpBtn1->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_timeout->setIcon(refresh_icon);
+	  ui_.helpBtn_timeout->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_timeout->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn2->setIcon(refresh_icon);
-	  ui_.helpBtn2->setIconSize(QSize(21, 21));
-	  ui_.helpBtn2->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_Dsleeptime->setIcon(refresh_icon);
+	  ui_.helpBtn_Dsleeptime->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_Dsleeptime->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn3->setIcon(refresh_icon);
-	  ui_.helpBtn3->setIconSize(QSize(21, 21));
-	  ui_.helpBtn3->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_DexeCount->setIcon(refresh_icon);
+	  ui_.helpBtn_DexeCount->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_DexeCount->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn4->setIcon(refresh_icon);
-	  ui_.helpBtn4->setIconSize(QSize(21, 21));
-	  ui_.helpBtn4->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_Dmovespeed->setIcon(refresh_icon);
+	  ui_.helpBtn_Dmovespeed->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_Dmovespeed->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn5->setIcon(refresh_icon);
-	  ui_.helpBtn5->setIconSize(QSize(21, 21));
-	  ui_.helpBtn5->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_DaccFactor->setIcon(refresh_icon);
+	  ui_.helpBtn_DaccFactor->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_DaccFactor->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn6->setIcon(refresh_icon);
-	  ui_.helpBtn6->setIconSize(QSize(21, 21));
-	  ui_.helpBtn6->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_maxSpeed->setIcon(refresh_icon);
+	  ui_.helpBtn_maxSpeed->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_maxSpeed->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn7->setIcon(refresh_icon);
-	  ui_.helpBtn7->setIconSize(QSize(21, 21));
-	  ui_.helpBtn7->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_DtwistFactor->setIcon(refresh_icon);
+	  ui_.helpBtn_DtwistFactor->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_DtwistFactor->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn8->setIcon(refresh_icon);
-	  ui_.helpBtn8->setIconSize(QSize(21, 21));
-	  ui_.helpBtn8->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_DtwistSpeed->setIcon(refresh_icon);
+	  ui_.helpBtn_DtwistSpeed->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_DtwistSpeed->setStyleSheet("QPushButton{border: none;outline: none;}");
 
-	  ui_.helpBtn9->setIcon(refresh_icon);
-	  ui_.helpBtn9->setIconSize(QSize(21, 21));
-	  ui_.helpBtn9->setStyleSheet("QPushButton{border: none;outline: none;}");
+	  ui_.helpBtn_DgripperStep->setIcon(refresh_icon);
+	  ui_.helpBtn_DgripperStep->setIconSize(QSize(21, 21));
+	  ui_.helpBtn_DgripperStep->setStyleSheet("QPushButton{border: none;outline: none;}");
 
+}
+
+void ccgui::activeSpeechCommand(QString command, QString data){
+	QLabel* label;
+	QLabel* labelData;
+	if(command == "move"){
+		label = ui_.label_move;
+		labelData = ui_.label_move_data;
+	}else if(command == "turn"){
+		label = ui_.label_turn;
+		labelData = ui_.label_turn_data;
+	}else if(command == "look"){
+		label = ui_.label_look;
+		labelData = ui_.label_look_data;
+	}else if(command == "grip"){
+		label = ui_.label_gripper;
+		labelData = ui_.label_grip_data;
+	}
+	label->setStyleSheet("QLabel{ gridline-color: rgb(0, 0, 0); "
+			"color: rgb(0,0,0); "
+			"padding: 2px; background-color: rgb(160, 160, 160);"
+			"border: 4px outset grey; } ");
+	labelData->setStyleSheet("QLabel{font: bold; font-size:24pt; color: rgb(0,0,204);}");
+	labelData->setText(data);
+}
+void ccgui::deactiveSpeechCommand(QString command){
+	QLabel* label;
+	QLabel* labelData;
+	if(command == "move"){
+		label = ui_.label_move;
+		labelData = ui_.label_move_data;
+	}else if(command == "turn"){
+		label = ui_.label_turn;
+		labelData = ui_.label_turn_data;
+	}else if(command == "look"){
+		label = ui_.label_look;
+		labelData = ui_.label_look_data;
+	}else if(command == "grip"){
+		label = ui_.label_gripper;
+		labelData = ui_.label_grip_data;
+	}
+	label->setStyleSheet("QLabel{ gridline-color: rgb(0, 0, 0); "
+			"color: rgb(64,64,64); "
+			"padding: 2px; background-color: rgb(96, 96, 96);"
+			"border: 4px inset grey; } ");
+	labelData->setStyleSheet("QLabel{ font-size:24pt; color: rgb(64,64,64);}");
+}
+
+void ccgui::clickLabelhelpBtn_timeout(){
+	 this->deactiveSpeechCommand("move");
+	  ros::Subscriber sub = getNodeHandle().subscribe("/speech_control_interface/cmd", 10, &ccgui::receiveSpeechCommand, this);
+	  ROS_INFO("topics %s %d", sub.getTopic().c_str(), 	  sub.getNumPublishers());
+//	 int ret = QMessageBox::information(widget_, QString("About timeout"),
+//			 QString("Timeout between two commands.\n"
+//	                                   "in [ms]"),
+//	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_Dsleeptime(){
+	 this->activeSpeechCommand("move", "backward");
+
+//	 int ret = QMessageBox::information(widget_, QString("About default sleep time"),
+//			 QString("The default sleep time between two execution steps.\n"
+//	                                   "in [s]"),
+//	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_DexeCount(){
+	 int ret = QMessageBox::information(widget_, QString("About default execution count"),
+			 QString("The default execution count of a command.\n"
+	                                   ),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_Dmovespeed(){
+	 int ret = QMessageBox::information(widget_, QString("About default move speed"),
+			 QString("The default move speed of the roboter.\n"
+	                                   "in [m/s]"),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_DaccFactor(){
+	 int ret = QMessageBox::information(widget_, QString("About default accelerator factor"),
+			 QString("Describes the additional speed, if the roboter accelerate.\n"
+	                                   "in [m/s]"),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_maxSpeed(){
+	 int ret = QMessageBox::information(widget_, QString("About maximal speed"),
+			 QString("The maximal speed of the roboter.\n"
+	                                   "in [m/s]"),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_DtwistFactor(){
+	 int ret = QMessageBox::information(widget_, QString("About default twist factor"),
+			 QString("Describes the turn/twist factor of the roboter.\n"
+	                                   "in ["+ QString::fromUtf8("°")+"]"),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_DtwistSpeed(){
+	 int ret = QMessageBox::information(widget_, QString("About default twist speed"),
+			 QString("The default twist/turn speed of the roboter.\n"
+	                                   "in [rad/s]"),
+	                                QMessageBox::Close);
+}
+void ccgui::clickLabelhelpBtn_DgripperStep(){
+	 int ret = QMessageBox::information(widget_, QString("About default gripper step."),
+			 QString("The default gripper step of the roboter arm.\n"
+	                                   ),
+	                                QMessageBox::Close);
 }
 
 } // namespace
